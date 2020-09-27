a04e722c86100b50a9acb909ce64eccf901657b6
Evan Alekseyev <54689704+ealekseyev@users.noreply.github.com>
2020-09-26 22:33:41 -0700

    boot into 32 bit: does not work
    under construction - add gdt.asm, boot32.asm

--- boot/bootsec.asm
+++ boot/bootsec.asm
@@ -14,14 +14,24 @@ section .text
 _start:
   mov si, Bootstrap
   call print
-  mov bx, 167
-  call printhex
-  jmp $
 
+; enable 20th bit - 1M memory
+  mov si, Bootstrap
+  call print
+  mov ax, 0x2401
+  int 0x15
+
+; setting up vga mode
+  mov ax, 0x3
+  int 0x10 ; set vga text mode 3
+ ; mov bx, 167
+ ; call printhex
+  jmp $
 
 %include "bprint.asm"
 
-Bootstrap: db "Loading S_OS...", 10, 13, "0x", 0
+Bootstrap: db "Loading S_OS...", 10, 13, 0
+EnableA20: db "Enabling A20 bit...", 10, 13, 0
 
 times 510-($-$$) db 0
 dw 0xaa55
--- boot/bprint.asm
+++ boot/bprint.asm
@@ -16,38 +16,39 @@ print:
     popa
     ret
 
+;TODO: nasm doesnt like this.
 ; convert and print int to hex
-printhex: ; take bx as argument for 16 bit int, print as hex form
-  push dx ; push pref data val to stack
-  push ax ; push prev ah and al vals to stack
-  push cl ; counter
-  mov ah, 0x0e ; put bios in print mode
-  mov cl, 12
-  call _printhexch
-  ret
-
-  _printhexch:
-    mov dx, bx ; copy value to dx
-    shl dx, 12-cl ;-cl ; get rid of hex before
-    shr dx, 12 ; remove all but 0 thru 16 = one hex digit
-    cmp dx, 10 ; will determine if the digit is a char or digit
-
-    jl _printdigit ; if less than 10, jump to printdigit
-    jmp _printchar ; else print the char
-    _printdigit:
-      mov al, dx+48 ; shift digit to ascii format
-      int 0x10
-      jmp _cycle
-    _printchar:
-      mov al, dx+87 ; 97 (ascii a) - 10 (regular number digits)
-      int 0x10
-      jmp _cycle
-    _cleanup:
-      pop dx ; reset all registers to prev state and exit _printhexch
-      pop ax
-      pop cl
-      ret
-    _cycle:
-      jcxz _cleanup ; jump if the counter is done (zero)
-      sub cl, 0x04 ; if counter is not done, subtract and jump back
-      jmp _printhexch
+;printhex: ; take bx as argument for 16 bit int, print as hex form
+;  push dx ; push pref data val to stack
+;  push ax ; push prev ah and al vals to stack
+;  push cl ; counter
+;  mov ah, 0x0e ; put bios in print mode
+;  mov cl, 12
+;  call _printhexch
+;  ret
+;
+;  _printhexch:
+;    mov dx, bx ; copy value to dx
+;    shl dx, 12-cl ;-cl ; get rid of hex before
+;    shr dx, 12 ; remove all but 0 thru 16 = one hex digit
+;    cmp dx, 10 ; will determine if the digit is a char or digit
+;
+;    jl _printdigit ; if less than 10, jump to printdigit
+;    jmp _printchar ; else print the char
+;    _printdigit:
+;      mov al, dx+48 ; shift digit to ascii format
+;      int 0x10
+;      jmp _cycle
+;    _printchar:
+;      mov al, dx+87 ; 97 (ascii a) - 10 (regular number digits)
+;      int 0x10
+;      jmp _cycle
+;    _cleanup:
+;      pop dx ; reset all registers to prev state and exit _printhexch
+;      pop ax
+;      pop cl
+;      ret
+;    _cycle:
+;      jcxz _cleanup ; jump if the counter is done (zero)
+;      sub cl, 0x04 ; if counter is not done, subtract and jump back
+;      jmp _printhexch
